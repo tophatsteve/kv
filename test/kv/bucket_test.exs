bc1e25615b39be956d60f6ad3e7679a254d8c64d
Steve Bentley <steve@codebazaar.co.uk>
2018-03-26 13:11:57 +0100

    Add delete function

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,11 +1,22 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values to key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values to key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes key from bucket, returning its current value", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "milk", 3)
+
+    assert KV.Bucket.delete(bucket, "milk") == 3
+    assert KV.Bucket.get(bucket, "milk") == nil
   end
 end
